ad336f3fca4f91d8d40daf32c7b65800903561e1
gabdreux <dreuxgabriel@hotmail.com>
2026-04-07 14:59:52 -0300

    feat(observability): implement CloudWatch EMF metrics, custom logger and IAM policies

--- src/handlers/processor.test.js
+++ src/handlers/processor.test.js
@@ -1,12 +1,25 @@
-import { handler } from './processor.js';
+import { jest } from '@jest/globals';
 import { mockClient } from 'aws-sdk-client-mock';
 import { DynamoDBDocumentClient, TransactWriteCommand } from '@aws-sdk/lib-dynamodb';
+
+jest.unstable_mockModule('../common/logger.js', () => ({
+    logMetric: jest.fn()
+}));
+
+const logger = await import('../common/logger.js');
+const { handler } = await import('./processor.js');
+
 
 process.env.LEDGER_TABLE = 'payment-ledger-table-test';
 const ddbMock = mockClient(DynamoDBDocumentClient);
 
+
+
 describe('Processor Handler - (Zod Validated)', () => {
-    beforeEach(() => ddbMock.reset());
+    beforeEach(() => {
+        ddbMock.reset();
+        jest.clearAllMocks();
+    });
 
     it('SUCCESS: should process a valid transfer with ACC# prefix', async () => {
         ddbMock.on(TransactWriteCommand).resolves({});
@@ -15,6 +28,8 @@ describe('Processor Handler - (Zod Validated)', () => {
         };
         const res = await handler(event);
         expect(res.statusCode).toBe(200);
+        expect(logger.logMetric).toHaveBeenCalledWith("ValidationSuccess", 1);
+        expect(logger.logMetric).toHaveBeenCalledWith("SuccessfulTransactions", 1);
     });
 
     it('SCHEMA ERROR: should reject account without ACC# prefix', async () => {
@@ -38,6 +53,7 @@ describe('Processor Handler - (Zod Validated)', () => {
         const res = await handler(event);
         expect(res.statusCode).toBe(400);
         expect(JSON.parse(res.body).message).toContain("Validation Error");
+        expect(logger.logMetric).toHaveBeenCalledWith("ValidationError", 1);
     });
 
     it('CONFLICT: should identify duplicate idempotency key', async () => {
@@ -52,6 +68,7 @@ describe('Processor Handler - (Zod Validated)', () => {
         const res = await handler(event);
         expect(res.statusCode).toBe(409);
         expect(JSON.parse(res.body).message).toBe("Duplicate transaction");
+        expect(logger.logMetric).toHaveBeenCalledWith("BusinessLogicError", 1);
     });
 
     it('RUNTIME ERROR: should handle malformed JSON', async () => {
@@ -59,6 +76,7 @@ describe('Processor Handler - (Zod Validated)', () => {
         const res = await handler(event);
         expect(res.statusCode).toBe(400);
         expect(JSON.parse(res.body).message).toBe("Invalid JSON format");
+        expect(logger.logMetric).toHaveBeenCalledWith("MalformedJSON", 1);
     });
 
 });
